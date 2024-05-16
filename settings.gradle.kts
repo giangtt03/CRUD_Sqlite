ca33363cae6738b99fe8ec824ec66a4664eb6c43
giangtt03 <giangtt653@gmail.com>
2024-05-16 22:13:10 +0700

    first

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PH27127_DAMLI_SP24"
+include(":app")
